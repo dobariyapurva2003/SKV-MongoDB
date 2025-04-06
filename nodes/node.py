45188feb12fcfc26eea234625ea77716c6d56f77
dobariyapurva2003 <107313472+dobariyapurva2003@users.noreply.github.com>
2025-04-06 23:12:42 +0530

    Delete nodes directory

--- nodes/node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self, id, role, data):
-        self.id = id
-        self.role = role
-        self.data = data
-
-    def sync(self, data):
-        self.data.update(data)
